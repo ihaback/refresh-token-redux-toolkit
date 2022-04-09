8ff7b4fa8f15231a8f1094c6c55b4ccd63a0a6b4
ihaback <ihgit@fastmail.com>
2022-04-09 15:41:50 +0200

    update types

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+import React, { PropsWithChildren } from "react";
 
-export const Layout: FC = ({ children }) => {
+export const Layout = ({ children }: PropsWithChildren<any>) => {
   return (
     <div className="flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 lg:pt-24">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
