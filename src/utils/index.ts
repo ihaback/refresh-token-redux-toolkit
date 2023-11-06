6426f2ac5ee3379beb24e8372230fb354a34e11b
ihaback <ihaback@fastmail.com>
2023-11-06 22:07:42 +0100

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { store } from "store";
 import { refreshToken } from "features";
-import jwt_decode from "jwt-decode";
+import jwtDecode from "jwt-decode";
 
 export const axiosPublic = axios.create({
   baseURL: "http://localhost:6060/api",
@@ -16,7 +16,7 @@ axiosPrivate.interceptors.request.use(
 
     let currentDate = new Date();
     if (user?.accessToken) {
-      const decodedToken: { exp: number } = jwt_decode(user?.accessToken);
+      const decodedToken: { exp: number } = jwtDecode(user?.accessToken);
       if (decodedToken.exp * 1000 < currentDate.getTime()) {
         await store.dispatch(refreshToken());
         if (config?.headers) {
