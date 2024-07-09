bbe4ae6d5095fb44ac1ffbc7defb7099bce667ea
ihaback <ihaback@fastmail.com>
2024-07-10 00:25:57 +0200

    adjust types

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from "react";
 
-export const Layout = ({ children }: PropsWithChildren<any>) => {
+export const Layout = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div className="flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 lg:pt-24">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
